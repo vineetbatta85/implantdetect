5dd0d36d68ce4b19e36abd2e511175051d0f86b0
lakshayyy10 <lakshayyy15@gmail.com>
2025-07-09 20:48:48 +0530

    prot

--- src/pages/Shoulder.tsx
+++ src/pages/Shoulder.tsx
@@ -13,7 +13,7 @@ const Shoulder = () => {
     formData.append('file', file);
 
     setLoading(true);
-    const res = await fetch('https://aiimaging-1.onrender.com/predict/', {
+    const res = await fetch('https://aiimaging.onrender.com/predict/', {
       method: 'POST',
       body: formData,
     });
