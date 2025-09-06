d660d42de9c012ee7d7745c6bcd13c620aa2bc79
lakshayyy10 <lakshayyy15@gmail.com>
2025-09-07 00:42:45 +0530

    Remove old LFS-tracked models

--- website_backend/try.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from tensorflow import keras
-from PIL import Image
-import io
-
-model = keras.models.load_model("knee.h5")
-print(model.output_shape)  # See how many classes it predicts
-
